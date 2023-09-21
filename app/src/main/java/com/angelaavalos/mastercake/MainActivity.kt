49e8804d5698bc165d0bb492c73717eeac37eb7c
Angela <angelaavalos2410@gmail.com>
2023-09-20 20:08:04 -0600

    testcommit

--- app/src/main/java/com/angelaavalos/mastercake/MainActivity.kt
+++ app/src/main/java/com/angelaavalos/mastercake/MainActivity.kt
@@ -4,20 +4,13 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.angelaavalos.mastercake.components.BottomNavBar
 import com.angelaavalos.mastercake.navigation.Destinations
 import com.angelaavalos.mastercake.navigation.NavigationHost
-import com.angelaavalos.mastercake.screens.FavoritesView
-import com.angelaavalos.mastercake.ui.theme.MASTERCAKETheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
